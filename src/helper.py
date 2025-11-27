750aa28fbf648a394d2410ecae4cc739ed093da4
Manohar Joshi <123627644+manohxr@users.noreply.github.com>
2025-11-27 15:14:23 +0530

    Update PyPDFLoader import from langchain_community

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from langchain.document_loaders import PyPDFLoader
+# from langchain.document_loaders import PyPDFLoader   old import
+from langchain_community.document_loaders import PyPDFLoader    #updated import
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
 from pinecone import Pinecone
